1f26094cd83868ed773a6b38765bce85e7617bcc
Amandathayane <amandathayaneas@gmail.com>
2025-06-06 17:52:29 -0300

    Adicionando as alterações no código

---
+++ cypress/e2e/sauceDemo/login.cy.js
@@ -0,0 +1,51 @@
+/// <reference types="cypress" />
+
+
+context('Validação da funcionalidade de login', () => {
+
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/v1/')
+        cy.clearCookies()
+    });
+
+
+    it('Validar os campos com email e senha válidos', () => {
+
+        //Passos
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('#login-button').click()
+
+        //Assertions
+        cy.get('.product_label').should('exist').and('be.visible')
+
+    });
+
+
+    it('Validar os campos com email e senha inválidos', () => {
+
+        //Passos
+        cy.get('[data-test="username"]').type('Amanda é linda')
+        cy.get('[data-test="password"]').type('amanda')
+        cy.get('#login-button').click()
+
+        //Assertions
+        cy.get('[data-test="error"]').should('exist').should('be.visible')
+        .should('have.text','Epic sadface: Username and password do not match any user in this service')
+
+    });
+
+    it('Validar os campos com email inválido', () => {
+
+        //Passos
+        cy.get('[data-test="username"]').type('Amanda é linda')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('#login-button').click()
+
+        //Assertions
+        cy.get('[data-test="error"]').should('exist').should('be.visible')
+        .should('have.text','Epic sadface: Username do not match any user in this service')
+
+    });
+
+});
---
+++ cypress/e2e/solicitacaoDeCredito/solicitacaoDeCredito.cy.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+
+
+context('Validação da funcionalidade de aprovação ou reprovação', () => {
+
+    beforeEach(() => {
+        cy.visit('https://angelofdiasg.tech/qaprogramador/sacfunc/')
+    });
+
+    it('Validar os campos obrigatórios', () => {
+
+        //Passos
+        cy.get('#nome').type('Amanda é linda')
+        cy.get('#email').type('amanda@hiago.com')
+        cy.get('#renda').type(2000)
+        cy.get('#cpf').type('123.456.789-11')
+        cy.get('#credito').type('1000')
+        cy.get('[type="submit"]').click()
+
+        //Assertions
+        cy.get('#result').should('exist').should('be.visible')
+        .should('have.text','Olá, Amanda é linda. Sua solicitação de crédito no valor de R$ 1000 foi APROVADA!')
+        .contains('APROVADA')
+
+    });
+
+
+    it('Validar aprovação de crédito', () => {
+        
+        //Passos
+        cy.get('#nome').type('Amanda é linda')
+        cy.get('#email').type('amanda@hiago.com')
+        cy.get('#renda').type(2000)
+        cy.get('#cpf').type('123.456.789-11')
+        cy.get('#credito').type('1000')
+        cy.get('[type="submit"]').click()
+
+        //Assertions
+        cy.get('#result').should('be.visible').should('contain', 'APROVADA')
+        .and('have.text','Olá, Amanda é linda. Sua solicitação de crédito no valor de R$ 1000 foi APROVADA!')
+    });
+
+
+    it('Validar reprovação de crédito', () => {
+        
+        //Passos
+        cy.get('#nome').type('Amanda é linda')
+        cy.get('#email').type('amanda@hiago.com')
+        cy.get('#renda').type(1000)
+        cy.get('#cpf').type('123.456.789-11')
+        cy.get('#credito').type('1000')
+        cy.get('[type="submit"]').click()
+
+        //Assertions
+        cy.get('#result').should('be.visible').should('contain', 'REPROVADA')
+
+        //.contains(/aprovada/i)
+        //.contains('Aprovada', {matchCase: false})
+    });
+});
